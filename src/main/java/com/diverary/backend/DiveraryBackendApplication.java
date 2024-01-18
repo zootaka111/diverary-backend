563edcdf778172bb96cff3c5db03525e83cbbe1f
zootaka <zootaka11013212@gmail.com>
2024-01-18 23:54:58 +0900

    [update]firstcommit

---
+++ src/main/java/com/diverary/backend/DiveraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diverary.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diverary/backend/DiveraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diverary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
